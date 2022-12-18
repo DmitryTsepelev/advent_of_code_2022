4285ec5f56ec987649bcc133228a84b3475d8840
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2022-12-18 12:55:08 +0300

    Day17 (p1)

--- day17/solution.hs
+++ day17/solution.hs
@@ -2,6 +2,7 @@
 import qualified Data.Set as S
 import Data.Maybe (fromJust)
 import Debug.Trace (trace)
+import Data.List (elemIndex)
 
 type Point = (Int, Int)
 type Chamber = S.Set Point
@@ -58,32 +59,44 @@ goDown chamber rock
   | otherwise = Nothing
   where newRock = moveDown rock
 
-simulateRockFall :: [Char] -> Chamber -> Rock -> ([Char], Chamber)
+simulateRockFall :: [(Int, Char)] -> Chamber -> Rock -> ([(Int, Char)], Chamber)
 simulateRockFall (jet:restJets) chamber rock =
   case maybeFallenRock of
     Just newRock -> simulateRockFall restJets chamber newRock
     Nothing -> (restJets, foldl (flip S.insert) chamber rockAfterJet)
   where
-    rockAfterJet = handleJet jet chamber rock
+    rockAfterJet = handleJet (snd jet) chamber rock
     maybeFallenRock = goDown chamber rockAfterJet
 
-throwRocks :: Int -> [RockType] -> [Char] -> Chamber -> Chamber
+throwRocks :: Int -> [(Int, RockType)] -> [(Int, Char)] -> Chamber -> Chamber
 throwRocks 0 _ jets chamber = chamber
 throwRocks n (rockType:rockTypes) jets chamber =
   throwRocks (n - 1) rockTypes newJets newChamber
   where
-    rock = findStartPosition rockType chamber
+    rock = findStartPosition (snd rockType) chamber
     (newJets, newChamber) = simulateRockFall jets chamber rock
 
-solve1 :: [RockType] -> [Char] -> Chamber -> Int
+solve1 :: [(Int, RockType)] -> [(Int, Char)] -> Chamber -> Int
 solve1 rockTypes jets chamber = maximum . map snd . S.elems $ throwRocks 2022 rockTypes jets chamber
 
--- throwRocks2 :: Int -> [(Int, RockType)] -> [(Int, Char)] -> Chamber -> Chamber
--- throwRocks2 n (rockType:rockTypes) jets chamber =
---   throwRocks2 (n + 1) rockTypes (map snd newJets) newChamber
---   where
---     rock = findStartPosition rockType chamber
---     (newJets, newChamber) = simulateRockFall jets chamber rock
+-- findLoop :: Int -> S.Set (Int, Int) -> [(Int, RockType)] -> [(Int, Char)] -> Chamber -> (Int, S.Set (Int, Int))
+findLoop n combinations (rockType:rockTypes) jets chamber
+  | combination `notElem` combinations = findLoop (n + 1) (combinations ++ [combination]) rockTypes newJets newChamber
+  | otherwise = (fromJust $ combination `elemIndex` combinations, n, combinations)
+  where
+    combination = (fst rockType, fst . head $ jets, getSurface chamber)
+
+    getSurface points =
+      map (\maximum -> maximum - minimum maximums) maximums
+      where
+        maximums = map (getMaximum points) [0..6]
+
+    getMaximum points x =
+      if null yValues then 0 else maximum yValues
+      where yValues = map snd $ filter (\(current, _) -> current == x) $ S.elems points
+
+    rock = findStartPosition (snd rockType) chamber
+    (newJets, newChamber) = simulateRockFall jets chamber rock
 
 drawChamber chamber = do
   mapM_ (putStrLn . row) $ reverse [1..maxY chamber + 8]
@@ -95,22 +108,35 @@ drawChamber chamber = do
       | point `elem` chamber = '#'
       | otherwise = '.'
 
+getHeight iterations rockTypes jets chamber =
+  if null heights then 0 else maximum heights
+  where heights = map snd $ S.elems $ throwRocks iterations rockTypes jets chamber
+
 main = do
   content <- readFile "input.txt"
 
-  let jets = concat . repeat . head . lines $ content
-  let rockTypes = concat . repeat $ [HorizontalLine, Cross, MirroredL, VerticalLine, Square]
+  let jets = concat . repeat . zip [0..] . head . lines $ content
+  let rockTypes = concat . repeat $ zip [0..] [HorizontalLine, Cross, MirroredL, VerticalLine, Square]
   let chamber = S.empty
 
   -- print $ "Solution 1 is " ++ show (solve1 rockTypes jets chamber)
 
-  -- let jetsLength = length (head . lines $ content)
-  -- let rockTypesLength = 4 * 5
-  -- let n = lcm rockTypesLength jetsLength
-  -- let rockCount = n `div` 4
-  -- print $ n
-  -- print $ rockCount
-  -- print $ 1000000000000 `mod` n
-  -- print $ (1000000000000 `div` rockCount)
-  print $ maximum $ map snd $ S.elems $ throwRocks 10 rockTypes jets chamber
-  -- print $ maximum $ map snd $ S.elems $ throwRocks 82 rockTypes jets chamber
+  let (cycleStart, cycleEnd, _) = findLoop 0 [] rockTypes jets chamber
+  print $ (cycleStart, cycleEnd)
+
+  let rocksCount = 1000000000000
+  -- let rocksCount = 2022
+  let heightAtCycleStart = getHeight cycleStart rockTypes jets chamber
+  -- print heightAtCycleStart
+  let heightAtCycleEnd = getHeight cycleEnd rockTypes jets chamber
+  -- print heightAtCycleEnd
+  -- 35
+  -- print $ getHeight 98 rockTypes jets chamber
+  -- print $ getHeight (98+35)  rockTypes jets chamber
+  let lastIteration =  max 0 ((rocksCount - cycleStart - 1) `mod` cycleEnd)
+  let lastIteration = 0
+  print $ "lastIteration " ++ (show lastIteration)
+  let lastUnfinishedCycleHeight = getHeight lastIteration rockTypes jets chamber
+  print $ lastUnfinishedCycleHeight
+
+  print $ getHeight (cycleStart - 1) rockTypes jets chamber + (heightAtCycleEnd - heightAtCycleStart) * (rocksCount `div` (cycleEnd - cycleStart)) + lastUnfinishedCycleHeight
